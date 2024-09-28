0ca13ddc900b1af58a839eef38c8ea4d99423464
kimjonghyuk <kasiruske@naver.com>
2024-09-28 22:53:11 +0900

    create middleware test

---
+++ app/Http/Middleware/CHeckUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CHeckUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        echo "ChekUser 미들웨어가 이 경로에 작성되었습니다.";
+        return $next($request);
+    }
+}
